8e1268e003683c1b2853bd65dc72fec3a08838cb
Ramzi Al-Dorikhim <ramzik149@gmail.com>
2022-10-15 14:00:47 +0300

    add hometask-4

---
+++ core/src/main/java/liga/medical/consumer/core/model/QueueNames.java
@@ -0,0 +1,11 @@
+package liga.medical.consumer.core.model;
+
+public class QueueNames {
+    public static final String ROUTER_QUEUE_NAME = "common_router_queue";
+
+    public static final String DAILY = "daily_queue";
+
+    public static final String ALERT = "alert_queue";
+
+    public static final String ERROR = "error_queue";
+}
